a16c44d12417cecaff426d192f14149b3df5fd62
sachin <sb786343@gmail.com>
2026-06-21 04:31:48 +0530

    shifed the proxy.js and index.js outside in a api folder in root changed the verce.js to be int it's simpliest form

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config(); // ✅ Load environment variables FIRST
-
-const app = require('./app');
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
-});
